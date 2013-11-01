f684e74ab118ead17050da29f62452795b62ea86
zjuchenjm <zjutchenjm@126.com>
2013-11-01 12:09:13 +0800

    1003

--- PAT/b1003.cpp
+++ PAT/b1003.cpp
@@ -5,23 +5,26 @@ using namespace std;
 int main()
 {
 	int n;
+	string ch;
+
 	cin>>n;
+
 	while(n > 0)
 	{
-		string str;
-		cin>>str;
+		cin>>ch;
+		--n;
 
 		int a = 0, b = 0, c = 0;
 		int indexP, indexT;
 		bool answer = true;
-		int len = str.length();
+		int len = ch.length();
 		for (int i = 0; i < len; ++i)
 		{
-			if (str[i] == 'A')
+			if (ch[i] == 'A')
 			{
 				++a;
 			}else{
-				if (str[i] != 'P')
+				if (ch[i] != 'P')
 				{
 					answer = false;
 				}
@@ -34,14 +37,14 @@ int main()
 			cout << "NO\n";
 			continue;
 		}
-		
+
 		for (int i = len-1; i >= 0; --i)
 		{
-			if (str[i] == 'A')
+			if (ch[i] == 'A')
 			{
 				++c;
 			}else{
-				if (str[i] != 'T')
+				if (ch[i] != 'T')
 				{
 					answer = false;
 				}
@@ -54,10 +57,16 @@ int main()
 			cout << "NO\n";
 			continue;
 		}
-		
+
+		if (indexP >= indexT)
+		{
+			cout << "NO\n";
+			continue;
+		}
+
 		for (int i = indexP + 1; i < indexT; ++i)
 		{
-			if (str[i] != 'A')
+			if (ch[i] != 'A')
 			{
 				answer = false;
 				break;
@@ -65,16 +74,38 @@ int main()
 			++b;
 		}
 
+		if (!answer)
+		{
+			cout << "NO\n";
+			continue;
+		}
+
 		while(c > a && b > 0)
 		{
 			c -= a;
 			--b;
 		}
+
 		if(c != a)
 		{
 			cout << "NO\n";
 		}else{
-			cout<< "YES\n";
+			if (a > 0)
+			{
+				if (b == 1)
+				{
+					cout<< "YES\n";
+				}else{
+					cout << "NO\n";
+				}
+			}else{
+				if (b > 0)
+				{
+					cout<< "YES\n";
+				}else{
+					cout << "NO\n";
+				}
+			}
 		}
 	}
 	system("pause");
